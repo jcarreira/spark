576413fc52636971fa72510a109c20b1b8dc60ac
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2014-12-06 23:28:24 -0800

    Checkpoint

--- examples/src/main/scala/org/apache/spark/examples/streaming/DataGeneratorTimestamp.scala
+++ examples/src/main/scala/org/apache/spark/examples/streaming/DataGeneratorTimestamp.scala
@@ -1,5 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.examples.streaming
+
 import java.io.IOException
 import java.net.ServerSocket
+import org.apache.spark.streaming.util.RateLimitedOutputStream
 
 import org.apache.spark.util.IntParam
 
@@ -27,11 +47,31 @@ object DataGeneratorTimestamp {
       println("Got a new connection")
       val out = new RateLimitedOutputStream(socket.getOutputStream, bytesPerSec)
       try {
-        while (true) {
-          val curTime = System.currentTimeMillis
-          val curTimeString = curTime.toString + "\n"
 
+        var timeBegin = System.currentTimeMillis
+        var sentBytes = 0
+
+        while (true) {
+
+          if (out.haveToWait()) {
+            val toWait = out.timeToWait();
+            println("Sleeping ms: " + toWait)
+            Thread.sleep(toWait + 5)
+          }
+
+          val curTime = System.currentTimeMillis
+          var curTimeString = ""
+          (1 to 1000).foreach(id => curTimeString += curTime.toString + "\n")
+
+          sentBytes += curTimeString.length
           out.write(curTimeString.getBytes)
+
+          // a second passed?
+          if (System.currentTimeMillis - timeBegin >= 1000) {
+              println("Last sec sent bytes: " + sentBytes)
+                sentBytes = 0
+                timeBegin = System.currentTimeMillis
+          }
         }
       } catch {
         case e: IOException =>
